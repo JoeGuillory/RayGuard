8bc9698cf13ab5fb773de7d85a877e89bc15aa10
JoeGuillory <joeguillory15@gmail.com>
2024-12-11 14:57:14 -0600

    Small Changes

--- RayGuard/src/Engine/Transform2D.cpp
+++ RayGuard/src/Engine/Transform2D.cpp
@@ -1,5 +1,6 @@
 #include "Transform2D.h"
 #include "Actor.h"
+#include "Matrix3.h"
 
 Transform2D::Transform2D() {}
 
@@ -10,6 +11,29 @@ Transform2D::Transform2D(Actor* owner)
 
 Transform2D::~Transform2D()
 {
+}
+
+//Public Functions
+void Transform2D::Translate(MathLibrary::Vector2 direction)
+{
+	
+}
+
+void Transform2D::Translate(float x, float y)
+{
+}
+
+void Transform2D::Rotate(float radians)
+{
+}
+
+void Transform2D::SetAngle(float radians)
+{
+}
+
+void Transform2D::UpdateTransforms()
+{
+	m_localMatrix
 }
 
 
--- RayGuard/src/Engine/Transform2D.h
+++ RayGuard/src/Engine/Transform2D.h
@@ -1,10 +1,11 @@
 #pragma once
-#include "Matrix3.h"
 #include "Vector2.h"
 
+namespace MathLibrary
+{
+	class Matrix3;
+}
 class Actor;
-
-using namespace MathLibrary;
 
 class Transform2D
 {
@@ -22,7 +23,8 @@ public:
 
 
 	//Getters and Setters
-	MathLibrary::Vector2 GetLocalPosition();
+	MathLibrary::Vector2 LocalPosition();
+	void LocalPosition(MathLibrary::Vector2 position);
 	MathLibrary::Vector2 GetLocalScale();
 	MathLibrary::Vector2 GetGlobalPosition();
 	MathLibrary::Vector2 GetGlobalScale();
@@ -35,11 +37,11 @@ public:
 
 private:
 	//Variables
-	Matrix3 m_localMatrix;
-	Matrix3 m_globalMatrix;
-	Matrix3 m_localTranslation;
-	Matrix3 m_localRotation;
-	Matrix3 m_localScale;
+	MathLibrary::Matrix3* m_localMatrix;
+	MathLibrary::Matrix3* m_globalMatrix;
+	MathLibrary::Matrix3* m_localTranslation;
+	MathLibrary::Matrix3* m_localRotation;
+	MathLibrary::Matrix3* m_localScale;
 
 	Transform2D* m_parent;
 	Transform2D** m_children;
