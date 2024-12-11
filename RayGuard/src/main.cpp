7eaf13cf1d7c0200b8b3f5cb261558d8c589998e
JoeGuillory <joeguillory15@gmail.com>
2024-12-10 19:09:15 -0600

    Game file made
    Made the game class. Added it to main and put the raylib window creation in game run

--- RayGuard/src/main.cpp
+++ RayGuard/src/main.cpp
@@ -1,20 +1,9 @@
 
 #include <iostream>
-#include "raylib.h"
-
+#include "Game.h"
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
-
-    CloseWindow();
-
+    Game game = Game();
+    game.Run();
     return 0;
 }
