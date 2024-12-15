ce5dc8732b8bf77fe224c2c3fc509f65bb21d11b
JoeGuillory <joeguillory15@gmail.com>
2024-12-15 16:03:02 -0600

    Update Game.cpp

--- RayGuard/src/Game.cpp
+++ RayGuard/src/Game.cpp
@@ -11,16 +11,13 @@ void Game::Run()
 {
 
     InitWindow(800, 450, "raylib [core] example - basic window");
+    
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-       
-        Actor bob = Actor((char*)"bob");
-        RAYLIB_H::Vector2 location = { bob.Transform->LocalPosition().x,bob.Transform->LocalPosition().y };
-        DrawRectangleV(location, {100,100},BLACK);
         EndDrawing();
     }
 
