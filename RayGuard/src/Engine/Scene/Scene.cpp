4e96aecadec601e7bed99d5ddcb9d0478dc5b56a
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-12-18 11:15:46 -0600

    fixed the double scene problem

---
+++ RayGuard/src/Engine/Scene/Scene.cpp
@@ -0,0 +1,34 @@
+#include "Scene.h"
+
+Scene::Scene()
+{
+	m_actors = new Actor * [0];
+}
+
+Scene::~Scene()
+{
+	delete m_actors;
+}
+
+void Scene::AddActor(Actor* actor)
+{
+}
+
+bool Scene::RemoveActor(Actor* actor)
+{
+	return false;
+}
+
+void Scene::Start()
+{
+}
+
+void Scene::Update(double deltaTime)
+{
+
+}
+
+void Scene::End()
+{
+
+}
--- RayGuard/src/Engine/Scene/Scene.h
+++ RayGuard/src/Engine/Scene/Scene.h
@@ -1,6 +1,20 @@
 #pragma once
 
+
 class Actor;
 
-class Scene;
+class Scene
+{
+public:
+	Scene();
+	~Scene();
 
+	void AddActor(Actor* actor);
+	bool RemoveActor(Actor* actor);
+	virtual void Start();
+	virtual void Update(double deltaTime);
+	virtual void End();
+
+private:
+	Actor** m_actors;
+};
