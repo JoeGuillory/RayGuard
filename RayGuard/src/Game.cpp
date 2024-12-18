f86acbcbbb93f88be747d3a46f01f81ecae7cec4
JoeGuillory <joeguillory15@gmail.com>
2024-12-17 18:48:48 -0600

    Transform Finished
    Everything works in the Transform
    Child parent Relationship Works

--- RayGuard/src/Game.cpp
+++ RayGuard/src/Game.cpp
@@ -12,21 +12,16 @@ void Game::Run()
 
     InitWindow(800, 450, "raylib [core] example - basic window");
     
-    Actor bob = Actor("Bob");
-    Actor* jeff = Actor::Instantiate(new Actor("Jeff"), bob.Transform, MathLibrary::Vector2(10, 10));
-    
+
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Hi", 190, 200, 20, LIGHTGRAY);
 
-        DrawRectangle(bob.Transform->LocalPosition().x, bob.Transform->LocalPosition().y, 20, 20, BLACK);
-        DrawRectangle(jeff->Transform->LocalPosition().x, jeff->Transform->LocalPosition().y, 20, 20, BLACK);
-        if (IsKeyDown(KEY_W))
-        {
-            bob.Transform->Translate(bob.Transform->GetForward() * .5);
-        }
+        
+      
         EndDrawing();
     }
 
