edb26b7394fc39fc23f24e5132ab8fd4f9d30780
Joshua-Production <joshuaespinosabusiness@outlook.com>
2025-01-08 16:34:08 -0600

    bullet update

--- RayGuard/src/Game/Bullet.h
+++ RayGuard/src/Game/Bullet.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Engine/Actor.h"
+
+class Bullet : public Actor
+{
+public:
+
+	float bulletSize;
+	float Speed;
+	void Start();
+	void Update(double deltaTime);
+	void End();
+
+};
